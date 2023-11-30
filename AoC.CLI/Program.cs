468a072b340f7d66dcf73c0703401ff491ffe845
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2023-11-30 22:16:57 +0000

    Initial commit for AoC 2023

---
+++ AoC.CLI/Program.cs
@@ -0,0 +1,62 @@
+using AoC;
+using static Crayon.Output;
+
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+switch (args.ElementAtOrDefault(0))
+{
+    case "--most-recent-day-title":
+        var mostRecent = SolverFactory.Instance.Solvers.Last(x => x.DayName != "");
+        Console.WriteLine($"Day {mostRecent.DayNumber}: {mostRecent.DayName}");
+        return;
+
+    case "--pull":
+        await AoC.CLI.PullPuzzleInputCommand.DoAsync(args);
+        return;
+}
+
+static void PrintTitle()
+{
+    Console.Clear();
+    Console.WriteLine("🎄 Shakey's AoC 2023 🌟");
+}
+
+PrintTitle();
+
+bool resume;
+var defaultDay = SolverFactory.Instance.DefaultDay;
+var cliDays = new Queue<string>(args.Length > 0 ? args : new[] { "" });
+do
+{
+    Console.WriteLine(Green($"Type day number, or blank for {defaultDay}, or 'list', or 'x' to exit"));
+    var dayNumber = cliDays.TryDequeue(out var cliDay) ? cliDay : Console.ReadLine() ?? "x";
+    dayNumber = string.IsNullOrWhiteSpace(dayNumber) ? defaultDay : dayNumber;
+
+    resume = dayNumber is not ("x" or "exit");
+    switch (resume)
+    {
+        case true when dayNumber == "list":
+        {
+            PrintTitle();
+            Console.WriteLine(string.Join(
+                Environment.NewLine,
+                SolverFactory.Instance.Solvers.Where(x => !string.IsNullOrEmpty(x.DayName)).Select(x => $"Day {x.DayNumber}: {x.DayName}")));
+            break;
+        }
+        case true:
+        {
+            PrintTitle();
+            var solver = SolverFactory.Instance.TryCreateSolver(dayNumber);
+            if (solver != null)
+            {
+                await solver.RunAsync();
+            }
+            else
+            {
+                Console.WriteLine(Red($"No solver for day '{Bright.Cyan(dayNumber)}'."));
+            }
+
+            break;
+        }
+    }
+} while (resume);
---
+++ README.md
@@ -0,0 +1,32 @@
+# 🎄 Shakey's AoC 2023 🌟
+
+[![CI workflow](https://github.com/robshakespeare/aoc2023/actions/workflows/CI-workflow.yml/badge.svg)](https://github.com/robshakespeare/aoc2023/actions/workflows/CI-workflow.yml)
+[![App releases](https://github.com/robshakespeare/aoc2023/actions/workflows/release-workflow.yml/badge.svg)](https://github.com/robshakespeare/aoc2023/actions/workflows/release-workflow.yml)
+
+Rob Shakespeare's solutions to the Advent of Code 2023 challenges at https://adventofcode.com/2023.
+
+
+### Shakey's AoC 2023 Releases
+* [Windows CLI download](https://github.com/robshakespeare/aoc2023/releases/latest/download/AoC.CLI.exe)
+
+
+### Prerequisites
+
+* [.NET 7.0 SDK](https://dotnet.microsoft.com/en-us/download/dotnet/7.0)
+* Optional: to be able to run the cake scripts, first: `dotnet tool restore`
+
+
+### Run
+
+To run the console application:
+
+```
+dotnet run --project AoC.CLI
+```
+
+
+### Test
+
+```
+dotnet test
+```
