05c7f7589834b44df8d7f4dfd9604bd2d08a9999
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2023-12-02 07:04:37 +0000

    Day 2 part 1 done

--- AoC.Tests/Day02/Day2SolverTests.cs
+++ AoC.Tests/Day02/Day2SolverTests.cs
@@ -6,7 +6,13 @@ public class Day2SolverTests
 {
     private readonly Day2Solver _sut = new();
 
-    private const string ExampleInput = @"";
+    private const string ExampleInput = """
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        """;
 
     [Test]
     public void Part1Example()
@@ -15,7 +21,7 @@ public class Day2SolverTests
         var part1ExampleResult = _sut.SolvePart1(ExampleInput);
 
         // ASSERT
-        part1ExampleResult.Should().Be(null);
+        part1ExampleResult.Should().Be(8);
     }
 
     [Test]
@@ -25,7 +31,8 @@ public class Day2SolverTests
         var part1Result = _sut.SolvePart1();
 
         // ASSERT
-        part1Result.Should().Be(null);
+        part1Result.Should().BeGreaterThan(889);
+        part1Result.Should().Be(2551);
     }
 
     [Test]
--- AoC/Day02/Day2Solver.cs
+++ AoC/Day02/Day2Solver.cs
@@ -1,16 +1,80 @@
 namespace AoC.Day02;
 
-public class Day2Solver : ISolver
+public partial class Day2Solver : ISolver
 {
-    public string DayName => "";
+    public string DayName => "Cube Conundrum";
 
-    public long? SolvePart1(string input)
-    {
-        return null;
-    }
+    public long? SolvePart1(string input) => ParseInput(input).Where(game => game.IsPossible).Sum(game => game.GameId);
 
     public long? SolvePart2(string input)
     {
         return null;
     }
+
+    //record Game(long GameId, Dictionary<string, int>[] SetsOfCubes)
+    //{
+    //    public bool IsPossible { get; } = SetsOfCubes.All(set =>
+    //        set.All(cube => cube.));
+    //}
+
+    record Game(long GameId, SetOfCubes[] SetsOfCubes)
+    {
+        public bool IsPossible { get; } = SetsOfCubes.All(x => x.IsPossible);
+    }
+
+    record SetOfCubes(CubeCount[] CubeCounts)
+    {
+        public bool IsPossible { get; } = CubeCounts.All(x => x.IsPossible);
+    }
+
+    record CubeCount(int Count, string Color)
+    {
+        public bool IsPossible { get; } = Count <= MaxCountPerColor[Color];
+    }
+
+    static readonly Dictionary<string, int> MaxCountPerColor = new()
+    {
+        { "red", 12 },
+        { "green", 13 },
+        { "blue", 14 },
+    };
+
+    //Game[] ParseInput(string input)
+    //{
+    //    return ParseGamesRegex().Matches(input).Select(match =>
+    //    {
+    //        var gameId = int.Parse(match.Groups["gameId"].Value);
+
+    //        var setsOfCubes = match.Groups["setsOfCubes"].Value.Split("; ").Select(
+    //            set => set.Split(", ").Select(cubes =>
+    //            {
+    //                var pair = cubes.Split(" ");
+    //                return new { count = int.Parse(pair[0]), color = pair[1] };
+    //            }).ToDictionary(x => x.color, x => x.count))
+    //        .ToArray();
+
+    //        return new Game(gameId, setsOfCubes);
+    //    }).ToArray();
+    //}
+
+    Game[] ParseInput(string input)
+    {
+        return ParseGamesRegex().Matches(input).Select(match =>
+        {
+            var gameId = int.Parse(match.Groups["gameId"].Value);
+
+            var setsOfCubes = match.Groups["setsOfCubes"].Value.Split("; ").Select(
+                set => new SetOfCubes(set.Split(", ").Select(cubes =>
+                {
+                    var pair = cubes.Split(" ");
+                    return new CubeCount(int.Parse(pair[0]), pair[1].Trim());
+                }).ToArray()))
+            .ToArray();
+
+            return new Game(gameId, setsOfCubes);
+        }).ToArray();
+    }
+
+    [GeneratedRegex(@"Game (?<gameId>\d+): (?<setsOfCubes>.+)")]
+    private static partial Regex ParseGamesRegex();
 }
