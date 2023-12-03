c003dd44ff635c4915d9cc558af9aff2ee0d67d4
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2023-12-03 13:34:55 +0000

    Revert "Day 3 - having a go at optimizing, but its not much quicker and complicates the code"
    This reverts commit d4565473d5f60b55d8ce1eab579ab67a5923f454.

--- AoC/Day03/Day3Solver.cs
+++ AoC/Day03/Day3Solver.cs
@@ -8,7 +8,7 @@ public partial class Day3Solver : ISolver
 
     public long? SolvePart1(string input)
     {
-        var (numbers, symbols, _) = ParseInput(input);
+        var (numbers, symbols) = ParseInput(input);
 
         var symbolAdjacencyMap = symbols.SelectMany(symbol => symbol.AdjacentPositions).ToHashSet().ToFrozenSet();
 
@@ -17,64 +17,50 @@ public partial class Day3Solver : ISolver
 
     public long? SolvePart2(string input)
     {
-        var map = ParseInput(input);
+        var (numbers, symbols) = ParseInput(input);
 
-        //var numberPositionMap = map.Numbers
-        //    .SelectMany(number => number.Positions.Select(position => new { position, number }))
-        //    .ToDictionary(x => x.position, x => x.number)
-        //    .ToFrozenDictionary();
+        var numberPositionMap = numbers
+            .SelectMany(number => number.Positions.Select(position => new { position, number }))
+            .ToDictionary(x => x.position, x => x.number)
+            .ToFrozenDictionary();
 
-        return map.Symbols
-            .Where(symbol => symbol.Char == '*')
-            .Select(candidateGear => new
+        return symbols
+            .Where(x => x.IsGear)
+            .Select(gear => new
             {
-                candidateGear,
-                adjacentNumbers = map.GetNumbers(candidateGear.AdjacentPositions).ToArray(),
-                //adjacentNumbers = candidateGear.AdjacentPositions
-                //    .Select(pos => numberPositionMap.TryGetValue(pos, out var number) ? number : null)
-                //    .Where(number => number != null)
-                //    .Select(number => number!)
-                //    .Distinct()
-                //    .ToArray()
+                gear,
+                adjacentNumbers = gear.AdjacentPositions
+                    .Select(pos => numberPositionMap.TryGetValue(pos, out var number) ? number : null)
+                    .Where(number => number != null)
+                    .Select(number => number!)
+                    .Distinct()
+                    .ToArray()
             })
             .Where(x => x.adjacentNumbers.Length == 2)
             .Select(x => x.adjacentNumbers[0].Value * x.adjacentNumbers[1].Value)
             .Sum();
     }
 
-    record Number(long Value, Vector2 TopLeft, int Length) /*: Element(Position)*/
+    record Number(long Value, Vector2 TopLeft, int Length)
     {
         public Vector2[] Positions { get; } = Enumerable.Range(0, Length).Select(x => TopLeft + new Vector2(x, 0)).ToArray();
 
         public bool IsPartNumber(ISet<Vector2> symbolAdjacencyMap) => Positions.Any(symbolAdjacencyMap.Contains);
     }
 
-    record Symbol(char Char, Vector2 Position) /*: Element(Position)*/
+    record Symbol(char Char, Vector2 Position)
     {
         public Vector2[] AdjacentPositions { get; } = GridUtils.DirectionsIncludingDiagonal.Select(dir => Position + dir).ToArray();
 
-        //public bool IsGear => Char == '*';
+        public bool IsGear => Char == '*';
     }
 
-    record Map(Number[] Numbers, Symbol[] Symbols, Number?[][] Grid)
+    static (Number[] Numbers, Symbol[] Symbols) ParseInput(string input)
     {
-        //public IEnumerable<TElement> GetElementsOfType<TElement>(IEnumerable<Vector2> positions) where TElement : Element =>
-        //    positions.Select(Grid.SafeGet).OfType<TElement>().Distinct();
-
-        public IEnumerable<Number> GetNumbers(IEnumerable<Vector2> positions) =>
-            positions.Select(Grid.SafeGet).OfType<Number>().Distinct();
-    }
-
-    static Map ParseInput(string input)
-    {
-        var lines = input.ReadLines().ToArray();
-        var width = lines.First().Length;
-        var grid = Enumerable.Range(0, lines.Length).Select(y => new Number?[width]).ToArray();
-
         var numbers = new List<Number>();
         var symbols = new List<Symbol>();
 
-        foreach (var (line, y) in lines.Select((line, y) => (line, y)))
+        foreach (var (line, y) in input.ReadLines().Select((line, y) => (line, y)))
         {
             foreach (Match match in ParseLineRegex().Matches(line))
             {
@@ -82,9 +68,7 @@ public partial class Day3Solver : ISolver
 
                 if (match.Groups["number"].Success)
                 {
-                    var number = new Number(long.Parse(match.Value), position, match.Length);
-                    numbers.Add(number);
-                    number.Positions.ToList().ForEach(pos => grid[(int)pos.Y][(int)pos.X] = number);
+                    numbers.Add(new Number(long.Parse(match.Value), position, match.Length));
                 }
                 else
                 {
@@ -93,9 +77,7 @@ public partial class Day3Solver : ISolver
             }
         }
 
-        //numbers.ForEach(number => number.Positions.ToList().ForEach(pos => grid[(int)pos.Y][(int)pos.X] = number));
-
-        return new Map([.. numbers], [.. symbols], grid);
+        return (numbers.ToArray(), symbols.ToArray());
     }
 
     [GeneratedRegex(@"(?<number>\d+)|[^.]", RegexOptions.Compiled)]
