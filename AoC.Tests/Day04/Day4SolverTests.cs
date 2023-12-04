c181a991478271a5ded2ada8672db812f88fda27
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2023-12-04 06:56:59 +0000

    Day 4 part 1 solved

--- AoC.Tests/Day04/Day4SolverTests.cs
+++ AoC.Tests/Day04/Day4SolverTests.cs
@@ -6,7 +6,14 @@ public class Day4SolverTests
 {
     private readonly Day4Solver _sut = new();
 
-    private const string ExampleInput = @"";
+    private const string ExampleInput = """
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        """;
 
     [Test]
     public void Part1Example()
@@ -15,7 +22,7 @@ public class Day4SolverTests
         var part1ExampleResult = _sut.SolvePart1(ExampleInput);
 
         // ASSERT
-        part1ExampleResult.Should().Be(null);
+        part1ExampleResult.Should().Be(13);
     }
 
     [Test]
@@ -25,7 +32,7 @@ public class Day4SolverTests
         var part1Result = _sut.SolvePart1();
 
         // ASSERT
-        part1Result.Should().Be(null);
+        part1Result.Should().Be(20855);
     }
 
     [Test]
--- AoC/Day04/Day4Solver.cs
+++ AoC/Day04/Day4Solver.cs
@@ -1,16 +1,32 @@
 namespace AoC.Day04;
 
-public class Day4Solver : ISolver
+public partial class Day4Solver : ISolver
 {
-    public string DayName => "";
+    public string DayName => "Scratchcards";
 
-    public long? SolvePart1(string input)
-    {
-        return null;
-    }
+    public long? SolvePart1(string input) => ParseInput(input).Sum(scratchcard => scratchcard.GetValue());
 
     public long? SolvePart2(string input)
     {
         return null;
     }
+
+    record Scratchcard(int CardId, int[] WinningsNumbers, int[] OurNumbers)
+    {
+        public int[] MatchingNumbers { get; } = WinningsNumbers.Intersect(OurNumbers).ToArray();
+
+        public long GetValue() => (long)Math.Pow(2, MatchingNumbers.Length - 1);
+    }
+
+    [GeneratedRegex(@"^Card +(?<cardId>\d+):(?<winningNumbers>[^|]+)\|(?<ourNumbers>[^|]+)$", RegexOptions.Multiline)]
+    private static partial Regex ParseInputRegex();
+
+    static Scratchcard[] ParseInput(string input) => ParseInputRegex().Matches(input)
+        .Select(match => new Scratchcard(
+            int.Parse(match.Groups["cardId"].Value),
+            ParseNumbers(match.Groups["winningNumbers"].Value),
+            ParseNumbers(match.Groups["ourNumbers"].Value)))
+        .ToArray();
+
+    static int[] ParseNumbers(string value) => value.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 }
