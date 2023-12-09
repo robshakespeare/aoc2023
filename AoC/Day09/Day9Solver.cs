1dba805f07073aedb299faf966478761e8ec3997
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2023-12-09 06:00:04 +0000

    Day 9 tidying

--- AoC/Day09/Day9Solver.cs
+++ AoC/Day09/Day9Solver.cs
@@ -4,37 +4,25 @@ public class Day9Solver : ISolver
 {
     public string DayName => "Mirage Maintenance";
 
-    public long? SolvePart1(string input)
-    {
-        var analyzedHistories = ParseInput(input).Select(AnalyzeHistory);
+    public long? SolvePart1(string input) => ParseInput(input).Sum(x => x.ExtrapolatedValue);
 
-        return analyzedHistories.Sum(x => x.ExtrapolatedValue);
-    }
-
-    public long? SolvePart2(string input)
-    {
-        var analyzedHistories = ParseInput(input).Select(AnalyzeHistory);
-
-        analyzedHistories.Dump();
-
-        return analyzedHistories.Sum(x => x.PreappendedValue);
-    }
+    public long? SolvePart2(string input) => ParseInput(input).Sum(x => x.PrependedValue);
 
     record AnalyzedHistory(long[][] Sequences)
     {
         public long ExtrapolatedValue { get; } = GetExtrapolatedValue(Sequences);
 
-        public long PreappendedValue { get; } = GetPreappendedValue(Sequences);
+        public long PrependedValue { get; } = GetPrependedValue(Sequences);
 
         static long GetExtrapolatedValue(long[][] sequences, int pointer = 0) =>
             pointer >= sequences.Length
                 ? 0
                 : sequences[pointer][^1] + GetExtrapolatedValue(sequences, pointer + 1);
 
-        static long GetPreappendedValue(long[][] sequences, int pointer = 0) =>
+        static long GetPrependedValue(long[][] sequences, int pointer = 0) =>
             pointer >= sequences.Length
                 ? 0
-                : sequences[pointer][0] - GetPreappendedValue(sequences, pointer + 1);
+                : sequences[pointer][0] - GetPrependedValue(sequences, pointer + 1);
     }
 
     static AnalyzedHistory AnalyzeHistory(long[] history)
@@ -70,5 +58,6 @@ public class Day9Solver : ISolver
         return results;
     }
 
-    static long[][] ParseInput(string input) => input.ReadLines().Select(line => line.Split(" ").Select(long.Parse).ToArray()).ToArray();
+    static IEnumerable<AnalyzedHistory> ParseInput(string input) =>
+        input.ReadLines().Select(line => line.Split(" ").Select(long.Parse).ToArray()).Select(AnalyzeHistory);
 }
