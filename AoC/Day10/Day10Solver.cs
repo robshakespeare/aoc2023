2db721ad8ac4d44613623639130e7b5cea8c5e61
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2023-12-10 07:47:34 +0000

    Refactored exploration to return path of the connections as well as tiles

--- AoC/Day10/Day10Solver.cs
+++ AoC/Day10/Day10Solver.cs
@@ -4,44 +4,46 @@ public class Day10Solver : ISolver
 {
     public string DayName => "Pipe Maze";
 
-    public long? SolvePart1(string input) => ParseInput(input).Explorers.Select(Explore).Select(path => path.Length).Max() / 2;
+    public long? SolvePart1(string input) => ParseInput(input).PipeStarts.Select(Explore).Select(path => path.Length).Max() / 2;
 
     public long? SolvePart2(string input)
     {
-        var (_, grid, explorers) = ParseInput(input);
+        var (grid, explorers) = ParseInput(input);
 
         var loop = Explore(explorers.First());
+
+        loop.ToStringGrid(x => x.Tile.Position, x => x.Tile.Char, '.', padding: 1).RenderGridToConsole();
 
         return null;
     }
 
-    static Tile[] Explore(Explorer explorer)
+    static PipeTile[] Explore(PipeTile pipeTile)
     {
-        var path = new List<Tile>();
+        var path = new List<PipeTile>();
 
         do
         {
-            path.Add(explorer.CurrentTile);
+            path.Add(pipeTile);
 
-            var nextPos = explorer.CurrentTile.Position + explorer.Direction;
-            var nextTile = explorer.Grid.Get(nextPos);
+            var nextPos = pipeTile.Tile.Position + pipeTile.Direction;
+            var nextTile = pipeTile.Grid.Get(nextPos);
             if (nextTile.Connections.Length != 2)
             {
                 throw new Exception("Next tile should always have only 2 connections");
             }
 
-            var connection = nextTile.Connections.Single(c => c == explorer.NextConnection);
+            var connection = nextTile.Connections.Single(c => c == pipeTile.Connection);
             var nextConnection = nextTile.Connections.Single(c => c != connection);
             var nextDirection = Vector2.Normalize(nextConnection - nextTile.Position);
 
-            explorer = new Explorer(nextTile, nextConnection, nextDirection, explorer.Grid);
+            pipeTile = new PipeTile(nextTile, nextConnection, nextDirection, pipeTile.Grid);
         }
-        while (explorer.CurrentTile.Char != 'S');
+        while (pipeTile.Tile.Char != 'S');
 
-        return path.ToArray();
+        return [.. path];
     }
 
-    static (Tile Start, Tile[][] Grid, Explorer[] Explorers) ParseInput(string input)
+    static (Tile[][] Grid, PipeTile[] PipeStarts) ParseInput(string input)
     {
         Tile? start = null;
 
@@ -77,13 +79,13 @@ public class Day10Solver : ISolver
         start = start with { Connections = startConnections };
         grid[(int)start.Position.Y][(int)start.Position.X] = start;
 
-        // Get our explorers
-        var explorers = start.Connections.Select(c => new Explorer(start, c, Vector2.Normalize(c - start.Position), grid)).ToArray();
+        // Get the pipe connections of the start tile
+        var pipeStarts = start.Connections.Select(c => new PipeTile(start, c, Vector2.Normalize(c - start.Position), grid)).ToArray();
 
-        return (start, grid, explorers);
+        return (grid, pipeStarts);
     }
 
-    record struct Explorer(Tile CurrentTile, Vector2 NextConnection, Vector2 Direction, Tile[][] Grid);
+    record struct PipeTile(Tile Tile, Vector2 Connection, Vector2 Direction, Tile[][] Grid);
 
     record Tile(Vector2 Position, char Char, Vector2[] Connections)
     {
