39124b809b8209d105d1136f1ad8bd0195cbc575
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2023-12-10 06:55:10 +0000

    Dy 10 Part 2 examples prepped

--- AoC.Tests/Day10/Day10SolverTests.cs
+++ AoC.Tests/Day10/Day10SolverTests.cs
@@ -49,13 +49,64 @@ public class Day10SolverTests
     }
 
     [Test]
-    public void Part2Example()
+    public void Part2Example1()
     {
         // ACT
-        var part2ExampleResult = _sut.SolvePart2("rs-todo: ExampleInput");
+        var part2Example1Result = _sut.SolvePart2("""
+            ...........
+            .S-------7.
+            .|F-----7|.
+            .||.....||.
+            .||.....||.
+            .|L-7.F-J|.
+            .|..|.|..|.
+            .L--J.L--J.
+            ...........
+            """);
 
         // ASSERT
-        part2ExampleResult.Should().Be(null);
+        part2Example1Result.Should().Be(4);
+    }
+
+    [Test]
+    public void Part2Example2()
+    {
+        // ACT
+        var part2Example2Result = _sut.SolvePart2("""
+            ..........
+            .S------7.
+            .|F----7|.
+            .||....||.
+            .||....||.
+            .|L-7F-J|.
+            .|..||..|.
+            .L--JL--J.
+            ..........
+            """);
+
+        // ASSERT
+        part2Example2Result.Should().Be(4);
+    }
+
+    [Test]
+    public void Part2Example3()
+    {
+        // ACT
+        var part2Example3Result = _sut.SolvePart2("""
+            .F----7F7F7F7F-7....
+            .|F--7||||||||FJ....
+            .||.FJ||||||||L7....
+            FJL7L7LJLJ||LJ.L-7..
+            L--J.L7...LJS7F-7L7.
+            ....F-J..F7FJ|L7L7L7
+            ....L7.F7||L7|.L7L7|
+            .....|FJLJ|FJ|F7|.LJ
+            ....FJL-7.||.||||...
+            ....L---J.LJ.LJLJ...
+            """);
+
+        // ASSERT
+        part2Example3Result.Should().Be(10);
     }
 
     [Test]
