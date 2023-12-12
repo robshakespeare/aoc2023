ee4e7e87bcad744a502789e4582b3e2f3dd49673
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2023-12-12 22:23:45 +0000

    Day 12 more code tidy

--- AoC/Day12/Day12Solver.cs
+++ AoC/Day12/Day12Solver.cs
@@ -46,22 +46,20 @@ public class Day12Solver : ISolver
 
             var count = Springs[0] switch
             {
-                '?' =>
-                    // at this point, the arrangement splits in to 2 possibilities, either an undamaged spring '.', or a damaged spring '#'
+                '?' => // at this point, the arrangement splits in to 2 possibilities, either an undamaged spring '.', or a damaged spring '#'
                     (this with { Springs = '.' + Springs[1..] }).CountPossibleArrangements() +
                     (this with { Springs = '#' + Springs[1..] }).CountPossibleArrangements(),
-                '#' =>
-                    // we are part (start, middle or end) of a group of damaged springs, so increment group count
+
+                '#' => // we are part (start, middle or end) of a group of damaged springs, so increment group count
                     new RowState(Springs[1..], ExpectedCounts, ContiguousDamagedSpringsCount + 1, Cache).CountPossibleArrangements(),
-                '.' when ContiguousDamagedSpringsCount > 0 =>
-                    // we have just finished a group, check its size is valid
+
+                '.' when ContiguousDamagedSpringsCount > 0 => // we have just finished a group, so check its size is valid
                     ExpectedCounts.Length > 0 && ExpectedCounts[0] == ContiguousDamagedSpringsCount
                         ? new RowState(Springs[1..], ExpectedCounts[1..], 0, Cache).CountPossibleArrangements()
                         : 0, // this arrangement isn't valid, so don't continue, drop out, and don't count it
-                '.' =>
-                    new RowState(Springs[1..], ExpectedCounts, 0, Cache).CountPossibleArrangements(),
-                var spring =>
-                    throw new InvalidOperationException($"Unexpected spring: {spring}")
+
+                '.' => new RowState(Springs[1..], ExpectedCounts, 0, Cache).CountPossibleArrangements(),
+                var spring => throw new InvalidOperationException($"Unexpected spring: {spring}")
             };
 
             Cache?.Add(cacheKey, count);
