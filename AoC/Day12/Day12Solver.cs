b0760531dae5d119e18350ab5e9f813a1a192f6c
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2023-12-12 22:19:20 +0000

    Day 12 refactoring and improving cache management

--- AoC/Day12/Day12Solver.cs
+++ AoC/Day12/Day12Solver.cs
@@ -4,34 +4,33 @@ public class Day12Solver : ISolver
 {
     public string DayName => "Hot Springs";
 
-    public long? SolvePart1(string input)
-    {
-        var rows = input.ReadLines()
-            .Select(RowState.Parse)
-            .ToArray();
-
-        return SumCountOfPossibleArrangements(rows);
-    }
+    public long? SolvePart1(string input) => SumCountOfPossibleArrangements(input);
 
     public long? SolvePart2(string input)
     {
         return null;
     }
 
-    static long SumCountOfPossibleArrangements(RowState[] rows)
+    static long SumCountOfPossibleArrangements(string input)
     {
-        ArrangementsCountsCache.Clear();
+        Dictionary<string, long> arrangementsCountsCache = [];
+        var rows = input.ReadLines()
+            .Select(line => RowState.Parse(line, arrangementsCountsCache))
+            .ToArray();
+
+        //ArrangementsCountsCache.Clear();
+        
         return rows.Sum(row => row.CountPossibleArrangements());
     }
 
-    static readonly Dictionary<string, long> ArrangementsCountsCache = [];
+    //static readonly Dictionary<string, long> ArrangementsCountsCache = [];
 
-    public record RowState(string Springs, int[] ExpectedCounts, string Path, int ContiguousDamagedSpringsCount)
+    public record RowState(string Springs, int[] ExpectedCounts, string Path, int ContiguousDamagedSpringsCount, Dictionary<string, long>? Cache)
     {
-        public static RowState Parse(string line)
+        public static RowState Parse(string line, Dictionary<string, long>? cache = null)
         {
             var split = line.Split(' ');
-            return new(split[0] + '.', split[1].Split(',').Select(int.Parse).ToArray(), "", 0);
+            return new(split[0] + '.', split[1].Split(',').Select(int.Parse).ToArray(), "", 0, cache);
         }
 
         public long CountPossibleArrangements()
@@ -44,7 +43,7 @@ public class Day12Solver : ISolver
 
             var cacheKey = $"{Springs}_{string.Join(',', ExpectedCounts)}_{ContiguousDamagedSpringsCount}";
 
-            if (ArrangementsCountsCache.TryGetValue(cacheKey, out var cachedCount))
+            if (Cache?.TryGetValue(cacheKey, out var cachedCount) == true)
             {
                 return cachedCount;
             }
@@ -53,23 +52,23 @@ public class Day12Solver : ISolver
             {
                 '?' =>
                     // at this point, the arrangement splits in to 2 possibilities, either an undamaged spring '.', or a damaged spring '#'
-                    new RowState('.' + Springs[1..], ExpectedCounts, Path, ContiguousDamagedSpringsCount).CountPossibleArrangements() +
-                    new RowState('#' + Springs[1..], ExpectedCounts, Path, ContiguousDamagedSpringsCount).CountPossibleArrangements(),
+                    (this with { Springs = '.' + Springs[1..] }).CountPossibleArrangements() +
+                    (this with { Springs = '#' + Springs[1..] }).CountPossibleArrangements(),
                 '#' =>
                     // we are part (start, middle or end) of a group of damaged springs, so increment group count
-                    new RowState(Springs[1..], ExpectedCounts, Path + '#', ContiguousDamagedSpringsCount + 1).CountPossibleArrangements(),
+                    new RowState(Springs[1..], ExpectedCounts, Path + '#', ContiguousDamagedSpringsCount + 1, Cache).CountPossibleArrangements(),
                 '.' when ContiguousDamagedSpringsCount > 0 =>
                     // we have just finished a group, check its size is valid
                     ExpectedCounts.Length > 0 && ExpectedCounts[0] == ContiguousDamagedSpringsCount
-                        ? new RowState(Springs[1..], ExpectedCounts[1..], Path + '.', 0).CountPossibleArrangements()
+                        ? new RowState(Springs[1..], ExpectedCounts[1..], Path + '.', 0, Cache).CountPossibleArrangements()
                         : 0, // this arrangement isn't valid, so don't continue, drop out, and don't count it
                 '.' =>
-                    new RowState(Springs[1..], ExpectedCounts, Path + '.', 0).CountPossibleArrangements(),
+                    new RowState(Springs[1..], ExpectedCounts, Path + '.', 0, Cache).CountPossibleArrangements(),
                 var spring =>
                     throw new InvalidOperationException($"Unexpected spring: {spring}")
             };
 
-            ArrangementsCountsCache[cacheKey] = count;
+            Cache?.Add(cacheKey, count);
 
             return count;
         }
