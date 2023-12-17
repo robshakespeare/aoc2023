b9b26e5d68a0b9e76855236c98c4d22c4180d6b2
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2023-12-17 22:33:17 +0000

    Day 17, part 1, hmmm

--- AoC.Tests/Day17/Day17SolverTests.cs
+++ AoC.Tests/Day17/Day17SolverTests.cs
@@ -7,7 +7,19 @@ public class Day17SolverTests
     private readonly Day17Solver _sut = new();
 
     private const string ExampleInput = """
-
+        2413432311323
+        3215453535623
+        3255245654254
+        3446585845452
+        4546657867536
+        1438598798454
+        4457876987766
+        3637877979653
+        4654967986887
+        4564679986453
+        1224686865563
+        2546548887735
+        4322674655533
         """;
 
     [Test]
@@ -17,7 +29,7 @@ public class Day17SolverTests
         var part1ExampleResult = _sut.SolvePart1(ExampleInput);
 
         // ASSERT
-        part1ExampleResult.Should().Be(null);
+        part1ExampleResult.Should().Be(102);
     }
 
     [Test]
--- AoC/Day17/Day17Solver.cs
+++ AoC/Day17/Day17Solver.cs
@@ -2,15 +2,70 @@ namespace AoC.Day17;
 
 public class Day17Solver : ISolver
 {
-    public string DayName => "";
+    public string DayName => "Clumsy Crucible";
 
     public long? SolvePart1(string input)
     {
-        return null;
+        // Thinking A* Search, try Dijkstra Search first
+        // Node has position, current direction, and up to 2 previous directions
+        // Cost is the heat loss value, i.e the number of the cell corresponding to the position
+
+        var grid = input.ReadLines().ToArray();
+        var goalPosition = new Vector2(grid[0].Length - 1, grid.Length - 1);
+
+        var search = new AStarSearch<CrucibleNode>(
+            getSuccessors: node =>
+            {
+                // Can go any direction, excluding backwards, and also at most three blocks in a single direction before it must turn
+
+                var mustTurn = node.Dir == node.Dir1 && node.Dir == node.Dir2;
+                var prevPos = node.Position + (node.Dir * -1);
+
+                return GridUtils.DirectionsExcludingDiagonal
+                    .Where(dir => !mustTurn || dir != node.Dir) // rule: at most three blocks in a single direction before it must turn
+                    .Select(dir => (pos: node.Position + dir, dir))
+                    .Where(x => x.pos != prevPos) // rule: can't go backwards
+                    .Select(x => (x.pos, x.dir, heatLoss: grid.TryGet(x.pos, out var c) ? (c - '0') : -1))
+                    .Where(x => x.heatLoss > -1)
+                    .Select(x => new CrucibleNode(x.pos, x.heatLoss, x.dir, node.Dir, node.Dir1));
+
+                //IEnumerable<Vector2> GetDirections()
+                //{
+                //    yield return node;
+                //}
+
+                //yield return node;
+
+                //if (node.Dir != null && node.Dir == node.Dir1 && node.Dir == node.Dir2)
+                //{
+
+                //}
+            },
+            node => MathUtils.ManhattanDistance(node.Position, goalPosition));
+
+        var path = search.FindShortestPath(
+            start: new CrucibleNode(new Vector2(0, 0), default, default, default, default),
+            goal: new CrucibleNode(goalPosition, default, default, default, default));
+
+        return path.TotalCost;
     }
 
     public long? SolvePart2(string input)
     {
         return null;
     }
+
+    //static IEnumerable<CrucibleNode> GetSuccessors(CrucibleNode node)
+    //{
+
+    //}
+}
+
+public sealed record CrucibleNode(Vector2 Position, int HeatLoss, Vector2 Dir, Vector2 Dir1, Vector2 Dir2) : IAStarSearchNode
+{
+    public int Cost => HeatLoss;
+
+    public bool Equals(CrucibleNode? other) => other != null && other.Position == Position;
+
+    public override int GetHashCode() => Position.GetHashCode();
 }
