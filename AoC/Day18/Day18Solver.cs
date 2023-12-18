cdeb7c3838e1fd664726e2ca9daa2a1d3644652c
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2023-12-18 21:22:05 +0000

    Day 18, part 1, hmmm.  Got an valid trench map output, now "the next step is to dig out the interior so that it is one meter deep as well"!

--- AoC/Day18/Day18Solver.cs
+++ AoC/Day18/Day18Solver.cs
@@ -2,10 +2,37 @@ namespace AoC.Day18;
 
 public class Day18Solver : ISolver
 {
-    public string DayName => "";
+    public string DayName => "Lavaduct Lagoon";
 
     public long? SolvePart1(string input)
     {
+        HashSet<Vector2> trenchMap = [];
+
+        var position = Vector2.Zero;
+        trenchMap.Add(position);
+
+        foreach (var line in input.ReadLines())
+        {
+            var split = line.Split(' ');
+            var dir = split[0] switch
+            {
+                "U" => GridUtils.North,
+                "D" => GridUtils.South,
+                "L" => GridUtils.West,
+                "R" => GridUtils.East,
+                _ => throw new Exception("Invalid dir: " + split[0])
+            };
+            var amount = int.Parse(split[1]);
+
+            for (var i = 0; i < amount; i++)
+            {
+                position += dir;
+                trenchMap.Add(position);
+            }
+        }
+
+        Console.WriteLine(string.Join(Environment.NewLine, trenchMap.ToStringGrid(p => p, _ => '#', '.')));
+
         return null;
     }
 
