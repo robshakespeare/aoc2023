3e51764053c5abbb9c1459229e14eb86034e89c9
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2024-01-01 23:35:32 +0000

    Day 19 refactoring and thinking re part 2

--- AoC/Day19/Day19Solver.cs
+++ AoC/Day19/Day19Solver.cs
@@ -6,51 +6,47 @@ public partial class Day19Solver : ISolver
 
     public long? SolvePart1(string input)
     {
-        var accepted = new FinalWorkflow("A");
-        var rejected = new FinalWorkflow("R");
-
-        var workflows = ParseWorkflowRegex().Matches(input)
-            .Select(match => new Workflow(
-                match.Groups["name"].Value,
-                match.Groups["rating"].Captures.Zip(match.Groups["op"].Captures, match.Groups["threshold"].Captures).Zip(match.Groups["destination"].Captures)
-                    .Select(x => Rule.Create(
-                        rating: x.First.First.Value.Single(),
-                        op: x.First.Second.Value.Single(),
-                        threshold: int.Parse(x.First.Third.Value),
-                        destination: x.Second.Value))
-                    .ToArray(),
-                match.Groups["defaultDestination"].Value))
-            .Concat([accepted, rejected])
-            .ToFrozenDictionary(x => x.Name, x => x);
-
-        var inflow = workflows["in"];
+        var pipeline = Pipeline.Parse(input);
 
         var parts = ParsePartRegex().Matches(input).Select(match =>
             new Part(match.Groups["rating"].Captures.Zip(match.Groups["value"].Captures).ToDictionary(x => x.First.Value.Single(), x => int.Parse(x.Second.Value)))).ToArray();
 
         foreach (var part in parts)
         {
-            inflow.Process(part, workflows);
+            pipeline.In.Process(part, pipeline.Workflows);
         }
 
-        return accepted.Parts.Sum(part => part.GetTotalRating());
+        return pipeline.Accepted.Parts.Sum(part => part.GetTotalRating());
     }
 
     public long? SolvePart2(string input)
     {
+        // We only care about being accepted, so mayeb we could run thorugh with "ranges", ultimately creating slices of each of the 4 ratings that would get accepted.
+        // So, initially we start with:
+        // x: 1 to 4000
+        // m: 1 to 4000
+        // a: 1 to 4000
+        // s: 1 to 4000
+        // Which would be 4000 * 4000 * 4000 * 4000 combinations
+        // Then we pass it through the pipeline, working out each eventuality
+
+
+
         return null;
     }
 
-    record Rule(char Rating, string Destination, Func<int, bool> Delegate)
+    record Rule(char Rating, char Op, int Threshold, string Destination)
     {
-        public bool IsMatch(Part part) => Delegate(part.Ratings[Rating]);
-
-        public static Rule Create(char rating, char op, int threshold, string destination) => new(rating, destination, op switch
+        public bool IsMatch(Part part)
         {
-            '<' => value => value < threshold,
-            '>' => value => value > threshold,
-            _ => throw new Exception("Invalid op: " + op)
-        });
+            var value = part.Ratings[Rating];
+            return Op switch
+            {
+                '<' => value < Threshold,
+                '>' => value > Threshold,
+                _ => throw new Exception("Invalid op: " + Op)
+            };
+        }
     }
 
     record Workflow(string Name, Rule[] Rules, string DefaultDestination)
@@ -59,7 +55,7 @@ public partial class Day19Solver : ISolver
             workflows[Rules.FirstOrDefault(rule => rule.IsMatch(part))?.Destination ?? DefaultDestination].Process(part, workflows);
     }
 
-    record FinalWorkflow(string Name) : Workflow(Name, [], "")
+    sealed record FinalWorkflow(string Name) : Workflow(Name, [], "")
     {
         public List<Part> Parts { get; } = [];
 
@@ -69,6 +65,37 @@ public partial class Day19Solver : ISolver
     record Part(Dictionary<char, int> Ratings)
     {
         public long GetTotalRating() => Ratings.Values.Sum();
+    }
+
+    record Range(int Start, int End);
+
+    record Pipeline(FrozenDictionary<string, Workflow> Workflows, FinalWorkflow Accepted, FinalWorkflow Rejected)
+    {
+        public Workflow In { get; } = Workflows["in"];
+
+        public static Pipeline Parse(string input)
+        {
+            var accepted = new FinalWorkflow("A");
+            var rejected = new FinalWorkflow("R");
+
+            var workflows = ParseWorkflowRegex().Matches(input)
+                .Select(match => new Workflow(
+                    match.Groups["name"].Value,
+                    match.Groups["rating"].Captures.Zip(match.Groups["op"].Captures, match.Groups["threshold"].Captures).Zip(match.Groups["destination"].Captures)
+                        .Select(x => new Rule(
+                            Rating: x.First.First.Value.Single(),
+                            Op: x.First.Second.Value.Single(),
+                            Threshold: int.Parse(x.First.Third.Value),
+                            Destination: x.Second.Value))
+                        .ToArray(),
+                    match.Groups["defaultDestination"].Value))
+                .Concat([accepted, rejected])
+                .ToFrozenDictionary(x => x.Name, x => x);
+
+            var inflow = workflows["in"];
+
+            return new Pipeline(workflows, accepted, rejected);
+        }
     }
 
     [GeneratedRegex("""(?<name>\w+){(?:(?<rating>\w)(?<op>[<>])(?<threshold>\d+):(?<destination>\w+),)+(?<defaultDestination>\w+)}""")]
